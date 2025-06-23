aead44b8b324bc9767b87e1bee46a79764dd6b07
Hanieh Haeri <91407046+hhaeri@users.noreply.github.com>
2025-06-22 17:28:09 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Author: Hanieh Haeri
+# Created: 01/20/2023
+
+# Configuration file to centralize paths and constants
+
+SEED = 24
+BATCH_SIZE = 16
+N_CLASSES = 6
+BACKBONE = 'resnet34'
+
+TRAIN_IMG_PATH = "/data/train/images/"
+TRAIN_MASK_PATH = "/data/train/masks/"
+
+VAL_IMG_PATH = "/data/val/images/"
+VAL_MASK_PATH = "/data/val/masks/"
